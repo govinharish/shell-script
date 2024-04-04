9b4afbd70b40f734b2c4962208fa0817fa257aa6
harish <govinharris@gmail.com>
2024-04-04 22:09:06 +0530

    variable3type

---
+++ type3variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+ 
+person1=$1
+person2=$2
+
+echo "$person1 : Hi $person2, Good morning"
+echo "$person2: good morning $person1"
+echo "$person1: How are you $person2"
+echo "$person2: I am fine, how are you $person1"
