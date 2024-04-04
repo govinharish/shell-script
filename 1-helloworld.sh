4cf18b7a041916bd43c5d3597153f5590f3c51d4
harish <govinharris@gmail.com>
2024-04-04 21:42:22 +0530

    Shell scripting 3rd day

--- 1-helloworld.sh
+++ 1-helloworld.sh
@@ -4,5 +4,5 @@ echo "hello world, I am learning shell scripting"
 
 echo "ramesh : Hi suresh, Good morning"
 echo "sure: good morning ramesh"
-echo :ramesh: How are you suresh"
+echo "ramesh: How are you suresh"
 echo "suresh: I am fine, how are you ramesh"
