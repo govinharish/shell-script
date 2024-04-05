d3e86d68a89ce0d2db39ae6f7d0add07a06d2a2a
harish <govinharris@gmail.com>
2024-04-05 12:15:38 +0530

    4th day training1

--- variable.sh
+++ variable.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-Date=$(date)
-
-echo "Hello, this script is executed at timestamp: $Date"
-
-
-echo "what is your name"
-
-read username
-
-echo "what is your password"
-
-read -s password
-
-echo "your name is $username"
-
-echo "your password is $password"
 
 
 Number1=$1
