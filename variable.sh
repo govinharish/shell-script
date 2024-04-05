cdfd8e772fb5d22d38ef8960d67c08aa1515a144
harish <govinharris@gmail.com>
2024-04-05 18:05:01 +0530

    4th day training1

--- variable.sh
+++ variable.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-#number1=$1
-
-#if [ $number1 -gt 10 ]
-#then
- #   echo "$number1 is greater than 10"
-#else 
- #   echo "$number1 is not greater than 10"
-#fi
-
-#how to install my sql
-
 UserId=$(id -u)
 
-if [ $UserId -ne 0]
+if [ $UserId -ne 0 ]
 then
   echo "Error :: please log in with root access"
-
-  else
+  exit 1  
+else
     echo "your are root user"
 fi
 
