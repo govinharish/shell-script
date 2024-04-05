9e8e34763f128867c7f64a222c0aaa704eadcce8
harish <govinharris@gmail.com>
2024-04-05 14:21:13 +0530

    4th day training1

--- variable.sh
+++ variable.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-number1=$1
+#number1=$1
 
-if [ $number1 -gt 10 ]
+#if [ $number1 -gt 10 ]
+#then
+ #   echo "$number1 is greater than 10"
+#else 
+ #   echo "$number1 is not greater than 10"
+#fi
+
+#how to install my sql
+
+UserId=$(id -u)
+if [ $UserId -ne 0]
 then
-    echo "$number1 is greater than 10"
-else 
-    echo "$number1 is not greater than 10"
+  echo "Error :: please log in with root access"
+
+  else
+    echo "your are root user"
 fi
+
+    yum install mysql -y
